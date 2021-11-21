107a6d8dbc4d0ce988e9752da761bd8b68510cd8
Bonganjalo Hadebe <bonganjaloh.hadebe@gmail.com>
2021-11-21 21:02:14 +0200

    feat(index): bind all to the server

--- index.ts
+++ index.ts
@@ -1,22 +1,27 @@
-const { ApolloServer, gql } = require("apollo-server");
-
+import { ApolloServer } from "apollo-server";
+import { typeDefs } from "./typeDefs/person.typeDef";
+import { resolvers } from "./resolvers/person.resolver";
+import { Person } from "./data-source/person.datasource";
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+// const typeDefs = gql`
+//   type Query {
+//     hello: String
+//   }
+// `;
 
 // Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: (root, args, context) => "Hello world!"
-  }
-};
+// const resolvers = {
+//   Query: {
+//     hello: (root, args, context) => "Hello world!"
+//   }
+// };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  dataSources: () => ({
+    person: new Person("https://swapi.dev/api/")
+  })
 });
 
 server.listen().then(({ url }) => {
